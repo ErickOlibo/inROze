e21b7f5864c9913c6df5856de9f0a5f3787f589c
Erick Olibo <erick.olibo@gmail.com>
2017-07-21 15:57:26 +0300

    Initial Commit

---
+++ InRoZe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InRoZe
+//
+//  Created by Erick Olibo on 21/07/2017.
+//  Copyright © 2017 Erick Olibo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
