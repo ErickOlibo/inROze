28c49fa2130dfce895337fed2f36d1b5551e32d8
Erick Olibo <erick.olibo@gmail.com>
2017-08-05 20:03:57 +0300

    imported Facebook SDK via cocoa pods updated the info.plist updated the appDelegate

--- InRoZe/AppDelegate.swift
+++ InRoZe/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,16 +18,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        // Override point for customization after application launch.
-        
-        // Select the initial landing Tab Bar item -> view controller
-        //let tabBar: UITabBarController = self.window?.rootViewController as! UITabBarController
-        //tabBar.selectedIndex = 1
-        
-        
-        
+        //Added FACEBOOK Stuff
+        SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
+
         return true
     }
+    
+    
+    //Added Func from FACEBOOK
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return SDKApplicationDelegate.shared.application(app, open:url, options:options)
+        
+        
+    }
+    
+    
+    
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
