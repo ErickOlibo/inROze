588f621c24af24952a6c58c482fca7a1d591ecda
Erick Olibo <erick.olibo@gmail.com>
2017-08-11 19:30:06 +0300

    set the retrieving of data from Core Data next Import info from Facebook to CoreData

--- InRoZe/FacebookRequest.swift
+++ InRoZe/FacebookRequest.swift
@@ -26,17 +26,21 @@ public class FacebookRequest
     
     // Sample places to ue
     let sampleEvents = ["100531250667584", "102851610417423", "1036341596468620", "104974153527931", "106338836720028"]
+    let tmpParam = [FBEvent.id, FBEvent.name, FBEvent.startTime, FBEvent.endTime, FBEvent.updatedTime]
+    
     let param = [FBEvent.id, FBEvent.name, FBEvent.startTime, FBEvent.endTime, FBEvent.updatedTime,
-                 FBEvent.cover, FBEvent.place]
+                 FBEvent.cover, FBEvent.place, FBEvent.descript]
     
     // Call Facebook Graph API
-    func requestEventsInfo(with ids: [String], parameters: [String]) {
+    func requestCurrentEventsInfo(from city: String) {
         
         let eventIDsString = sampleEvents.joined(separator: ",")
-        let params = param.joined(separator: ", ")
+        //let params = param.joined(separator: ", ")
+        let params2 = tmpParam.joined(separator: ", ")
+
         
         // Background with Completion check for MEMORY CYCLE with [weak self]
-        FBSDKGraphRequest(graphPath: "/?ids=\(eventIDsString)", parameters: ["fields" : params])
+        FBSDKGraphRequest(graphPath: "/?ids=\(eventIDsString)", parameters: ["fields" : params2])
             .start(completionHandler:  { (connection, result, error) in
                 
                 if error != nil,  let result = result as? NSDictionary{
@@ -47,6 +51,24 @@ public class FacebookRequest
                 }
             })
         
+    }
+    
+    func printEventIDsFromCoreData() {
+        let context = container.viewContext
+        context.perform {
+            let request: NSFetchRequest<Event> = Event.fetchRequest()
+            if let events = try? context.fetch(request) {
+                var eventCount = 1
+                for event in events as [Event] {
+                    
+                    if let eventStr = event.id {
+                        print("\(eventCount)) \(eventStr)")
+                    }
+                    eventCount += 1
+                    
+                }
+            }
+        }
     }
     
     
--- InRoZe/FavoriteViewController.swift
+++ InRoZe/FavoriteViewController.swift
@@ -22,10 +22,9 @@ class FavoriteViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         print("Favorite")
-        let request = FacebookRequest()
-        let ids = request.sampleEvents
-        let param = request.param
-        _ = request.requestEventsInfo(with: ids, parameters: param)
+        
+        //_ = FacebookRequest().requestCurrentEventsInfo(from: "TLN")
+        _ = FacebookRequest().printEventIDsFromCoreData()
         
     }
  
