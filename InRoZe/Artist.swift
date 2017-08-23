5d79c943d3dec9c134b7e52417e0b4298ec9e033
Erick Olibo <erick.olibo@gmail.com>
2017-08-23 19:46:23 +0300

    adding core data entity: Artist

---
+++ InRoZe/Artist.swift
@@ -0,0 +1,19 @@
+//
+//  Artist.swift
+//  InRoZe
+//
+//  Created by Erick Olibo on 23/08/2017.
+//  Copyright © 2017 Erick Olibo. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+public class Artist: NSManagedObject
+{
+    // define particular functions for the coreData class
+
+    
+    
+    
+}
