c20a57decee9fcc02e1f59d14c85d446122906d6
Erick Olibo <erick.olibo@gmail.com>
2017-09-26 03:17:53 +0200

    list and follow color and number of gigs set and done

--- InRoZe/EventDJsCollectionViewCell.swift
+++ InRoZe/EventDJsCollectionViewCell.swift
@@ -16,6 +16,9 @@ class EventDJsCollectionViewCell: UICollectionViewCell
     
     let followedColor: UIColor = UIColor.changeHexStringToColor(ColorInHexFor.logoRed)
     let notFollowedColor: UIColor = .lightGray
+    let moreGigsString = " more gigs"
+    let moreGigString = " more gig"
+    let noGigString = "no other gig"
 
     // Outlets for Cell UI
     @IBOutlet weak var textDisplayBg: UIView! {
@@ -54,6 +57,8 @@ class EventDJsCollectionViewCell: UICollectionViewCell
     
     
     @IBOutlet weak var firstLetter: UILabel!
+    @IBOutlet weak var djName: UILabel!
+    @IBOutlet weak var moreGigs: UILabel!
     
     
     
@@ -70,6 +75,7 @@ class EventDJsCollectionViewCell: UICollectionViewCell
     
     private func updateUI() {
         firstLetter.text = String((thisDJ!.name!.uppercased()).characters.first!)
+        djName.text = thisDJ!.name!.uppercased()
         setIsFollowButton(for: thisDJ!.isFollowed)
         updateCellColorforFollowed(for: thisDJ!.isFollowed)
         
@@ -77,6 +83,7 @@ class EventDJsCollectionViewCell: UICollectionViewCell
     
     private func setIsFollowButton(for state: Bool) {
         var followImg = UIImage()
+        moreGigsString(for: state)
         updateCellColorforFollowed(for: state)
         if (state) {
             followImg = (UIImage(named: "2_FollowsFilled")?.withRenderingMode(.alwaysTemplate))!
@@ -89,9 +96,40 @@ class EventDJsCollectionViewCell: UICollectionViewCell
         }
         
     }
+    
+    private func moreGigsString(for state: Bool) {
+        let numberGigs = thisDJ!.gigs!.count - 1
+        let numberGigsAttributed: NSAttributedString
+        if (state) {
+            numberGigsAttributed = coloredString(String(numberGigs), color: followedColor)
+        } else {
+            numberGigsAttributed = coloredString(String(numberGigs), color: notFollowedColor)
+        }
+        
+        let moreGigsLabelAttributed = NSMutableAttributedString()
+        switch numberGigs {
+        case 0:
+            break
+            //moreGigs.attributedText = coloredString(noGigString, color: notFollowedColor)
+        case 1:
+            let otherGigsAttributedText = coloredString(moreGigString, color: notFollowedColor)
+            moreGigsLabelAttributed.append(numberGigsAttributed)
+            moreGigsLabelAttributed.append(otherGigsAttributedText)
+            moreGigs.attributedText = moreGigsLabelAttributed
+        default:
+            let otherGigsAttributedText = coloredString(moreGigsString, color: notFollowedColor)
+            moreGigsLabelAttributed.append(numberGigsAttributed)
+            moreGigsLabelAttributed.append(otherGigsAttributedText)
+            moreGigs.attributedText = moreGigsLabelAttributed
+        }
+        
+    }
+    
+    
    
     private func updateCellColorforFollowed(for state: Bool) {
         let djInitial = String((thisDJ!.name!.uppercased()).characters.first!)
+        let thisDJname = thisDJ!.name!.uppercased()
         if (state) {
             textDisplayBg.layer.borderColor = followedColor.cgColor
             outerCircle.layer.borderColor = followedColor.cgColor
@@ -101,6 +139,7 @@ class EventDJsCollectionViewCell: UICollectionViewCell
             textDisplayGigs.layer.borderColor = followedColor.cgColor
             followDJBottonView.layer.borderColor = followedColor.cgColor
             firstLetter.attributedText = coloredString(djInitial, color: .white)
+            djName.attributedText = coloredString(thisDJname, color: .black)
             
             
         } else {
@@ -111,7 +150,8 @@ class EventDJsCollectionViewCell: UICollectionViewCell
             innerCircle.backgroundColor = .white
             textDisplayGigs.layer.borderColor = notFollowedColor.cgColor
             followDJBottonView.layer.borderColor = notFollowedColor.cgColor
-            firstLetter.attributedText = coloredString(djInitial, color: .black)
+            firstLetter.attributedText = coloredString(djInitial, color: notFollowedColor)
+            djName.attributedText = coloredString(thisDJname, color: notFollowedColor)
             
             
         }
