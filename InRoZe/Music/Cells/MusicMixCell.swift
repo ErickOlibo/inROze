63b498b7b8e42ef0f7d113d8922d266ab06d6a39
Erick Olibo <erick.olibo@gmail.com>
2018-01-04 16:00:02 +0100

    collection is up and running put pictures of mixtape

---
+++ InRoZe/Music/Cells/MusicMixCell.swift
@@ -0,0 +1,36 @@
+//
+//  MusicMixCell.swift
+//  InRoZe
+//
+//  Created by Erick Olibo on 04/01/2018.
+//  Copyright © 2018 Erick Olibo. All rights reserved.
+//
+
+import UIKit
+
+class MusicMixCell: UICollectionViewCell
+{
+    
+    // properties
+    static var identifier: String { return String(describing: self) }
+    
+    
+    // outlets
+    @IBOutlet weak var thisLabel: UILabel! { didSet { updateUI() } }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //print("[\(tag)] - MusicMixCell woke from Nib")
+        
+        
+
+    }
+    
+    private func updateUI() {
+        thisLabel.text = String(tag)
+        print("[\(tag)] - MusicMixCell updateUI")
+    }
+    
+    
+}
---
+++ InRoZe/Music/View Controllers/MusicViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MusicViewController.swift
+//  InRoZe
+//
+//  Created by Erick Olibo on 04/01/2018.
+//  Copyright © 2018 Erick Olibo. All rights reserved.
+//
+
+import UIKit
+
+//private let reuseIdentifier = "Cell"
+
+class MusicViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        //self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+ 
+
+}
+
+extension MusicViewController: UICollectionViewDelegateFlowLayout
+{
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 200
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MusicMixCell.identifier, for: indexPath) as! MusicMixCell
+        cell.tag = indexPath.row
+        cell.thisLabel.text = String(indexPath.row)
+        print("cellForItemAt indexPath [\(indexPath)]")
+        // Configure the cell
+        
+        return cell
+    }
+    
+    
+}
