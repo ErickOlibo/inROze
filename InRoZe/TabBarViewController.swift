79e54207bdfc2c0d50818caaeb28b7c5de2e8792
Erick Olibo <erick.olibo@gmail.com>
2018-01-28 14:03:35 +0200

    implemented the going back to top at pressing the same tab

--- InRoZe/TabBarViewController.swift
+++ InRoZe/TabBarViewController.swift
@@ -10,58 +10,86 @@ import UIKit
 import AVFoundation
 
 
-class TabBarViewController: UITabBarController {
+class TabBarViewController: UITabBarController, UITabBarControllerDelegate {
     
     public var sharedPlayer = AVPlayer()
     public var isShowingMiniPlayer: Bool = false
+    private var isSameTab: Bool = false
     
+    
+    // View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.delegate = self
         //view.backgroundColor = .black
         print("[TabBarViewController] -- View DID Load")
         self.tabBar.isTranslucent = true
         self.tabBar.tintColor = Colors.logoRed
         //listFonts()
-
-
     }
     
 
-    
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//        print("[TabBarViewController] -- View WILL Appear")
-//    }
-//    
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        print("[TabBarViewController] -- View DID Appear")
-//    }
-//    
-//    
-//    override func viewWillDisappear(_ animated: Bool) {
-//        super.viewWillDisappear(animated)
-//        print("[TabBarViewController] -- View WILL Disappear")
-//    }
-//    
-//    override func viewDidDisappear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        print("[TabBarViewController] -- View DID Disappear")
-//    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+
     
     
     private func listFonts() {
         let fontFamilyNames = UIFont.familyNames
-        
         for familyName in fontFamilyNames {
-            
             print("Font Family Name = [\(familyName)]")
             let names = UIFont.fontNames(forFamilyName: familyName)
             print("Font Names = [\(names)]")
         }
     }
     
-    
-    
 
 }
+
+
+extension TabBarViewController
+{
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        guard let indexOfTab = tabBar.items?.index(of: item) else { return }
+        let index = Int(indexOfTab)
+        isSameTab = (selectedIndex == index) ? true : false
+    }
+    
+    
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        guard let tempNavC = viewController as? UINavigationController else { return }
+        if (isSameTab) {
+            print("Clicked on the same Tab")
+            if let root = tempNavC.viewControllers[0] as? EventsViewController {
+                root.tableView.setContentOffset(.zero, animated: true)
+            }
+            if let root = tempNavC.viewControllers[0] as? FollowsViewController {
+                root.tableView.setContentOffset(.zero, animated: true)
+            }
+            if let root = tempNavC.viewControllers[0] as? MusicViewController {
+                root.collectionView?.setContentOffset(.zero, animated: true)
+            }
+            if let root = tempNavC.viewControllers[0] as? DeejaysViewController {
+                root.tableView.setContentOffset(.zero, animated: true)
+            }
+        }
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
